cf7b20b054f5f598af837e1db3c058617156c71f
Andreas Kupries <akupries@shaw.ca>
2013-04-19 17:53:28 +0000

    Added higher level methods to create/verify an entire schema. table_info result structure changed, list over dict.

--- dbutil.tcl
+++ dbutil.tcl
@@ -32,9 +32,68 @@ proc dbutil::table_info {db table} {
     $db eval "PRAGMA table_info($table)" ti {
 	set entry [array get ti]
 	dict unset entry *
-	lappend tinfo $entry
+	# fields:
+	# cid
+	# name
+	# type
+	# notnull
+	# dflt_value
+	# pk
+	dict with entry {} ; # => fields as variables.
+	lappend tinfo [list $name $type $notnull $dflt_value $pk]
     }
     return $tinfo
+}
+
+proc dbutil::check {db table spec evar} {
+    upvar 1 $evar reason
+    set ti [table_info $db $table]
+    set nex [llength $spec]
+    set nis [llength $ti]
+    if {$nis != $nex} {
+	set reason "Expected $nex columns, got $nis"
+	return 0
+    }
+    foreach entry $ti sentry $spec {
+	if {![Match $table $entry $sentry reason]} { return 0 }
+    }
+    return 1
+}
+
+proc dbutil::initialize-schema {db evar args} {
+    upvar 1 $evar reason
+    # args = schema = dict (table -> def)
+    # where def = list (sql, table-info-spec)
+
+    dict for {table def} $args {
+	lassign $def sql info
+	if {[has $db $table]} {
+	    return [check $db $table $info reason]
+	}
+	$db transaction {
+	    $db eval "CREATE TABLE \"$table\" ( $sql )"
+	}
+    }
+    return 1
+}
+
+# # ## ### ##### ######## ############# #####################
+
+proc dbutil::Match {table entry spec evar} {
+    upvar 1 $evar reason
+
+    foreach e $entry s $spec l {name type notnull dflt_value pk} {
+	set $l $e
+	if {![string match $s $e]} {
+	    if {[info exists name]} {
+		set reason "$table.$name: $l mismatch. Expected \"$s\", got \"$e\""
+	    } else {
+		set reason "$table: $l mismatch. Expected \"$s\", got \"$e\""
+	    }
+	    return 0
+	}
+    }
+    return 1
 }
 
 # # ## ### ##### ######## ############# #####################
